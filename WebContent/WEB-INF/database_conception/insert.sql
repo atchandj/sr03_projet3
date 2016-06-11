0303ff37a09f7cdd961941762c19433818e8e4d8
d@ni€| <artchounindaniel@gmail.com>
2016-06-11 22:46:27 +0200

    Updated 'insert.sql'.

--- WebContent/WEB-INF/database_conception/insert.sql
+++ WebContent/WEB-INF/database_conception/insert.sql
@@ -1,13 +1,15 @@
-INSERT INTO `yearbook` VALUES (1,"annonce");
+INSERT INTO YearBook VALUES (1,"annonce");
 
-INSERT INTO `category` VALUES (6,"association"),
+INSERT INTO Category VALUES 
+(6,"association"),
 (3,"centre commercial"),
 (4,"cinéma"),
 (2,"etude"),
 (1,"football"),
 (5,"université");
 
-INSERT INTO `address` VALUES (6,"16 rue conrad adenauer","rosny-sous-bois","93110"),
+INSERT INTO Address VALUES 
+(6,"16 rue conrad adenauer","rosny-sous-bois","93110"),
 (5,"2 rue corentin cariou","paris","75014"),
 (2,"2 rue du dépôt","compiègne","60200"),
 (3,"3 avenue de la résistance","le raincy","93340"),
@@ -16,10 +18,13 @@ INSERT INTO `address` VALUES (6,"16 rue conrad adenauer","rosny-sous-bois","9311
 (7,"rue du dr schweitzer","compiègne","60200"),
 (8,"rue roger couttolenc","compiègne","60200");
 
-INSERT INTO `ad` VALUES (1,"club de football","0150876022",1,1),
+INSERT INTO Ad VALUES 
+(1,"club de football","0150876022",1,1),
 (1,"fans de foot","0145823600",1,2),
-(1,"le majestic","0150687750",4,5),(1,"pic\'asso","0155789033",6,8),
-(1,"révisions bac","0682453399",2,3),(1,"rosny 2‎","0148546319",3,4),
+(1,"le majestic","0150687750",4,5),(
+1,"pic\'asso","0155789033",6,8),
+(1,"révisions bac","0682453399",2,3),
+(1,"rosny 2‎","0148546319",3,4),
 (1,"ugc rosny 2","0176647997",4,6),
 (1,"université de technologie de compiègne","0344234423",5,7),
 (1,"usec","0344234362",6,8);
