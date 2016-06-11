df193709b34c7ee762028083e65d4b36677e1300
atchandj <atchandjou@gmail.com>
2016-06-11 22:19:01 +0200

    Udate data sets

--- WebContent/WEB-INF/database_conception/insert.sql
+++ WebContent/WEB-INF/database_conception/insert.sql
@@ -1,18 +1,25 @@
-INSERT INTO YearBook(name)
-VALUES('annonce');
+INSERT INTO `yearbook` VALUES (1,"annonce");
 
-INSERT INTO Category(name)
-VALUES("football"), ("etude");
+INSERT INTO `category` VALUES (6,"association"),
+(3,"centre commercial"),
+(4,"cinéma"),
+(2,"etude"),
+(1,"football"),
+(5,"université");
 
-INSERT INTO Address(street, town, postCode)
-VALUES("30 rue de clamart", "compiègne", "60200"),
-("2 rue du dépôt", "compiègne", "60200"),
-("3 avenue de la résistance", "le raincy", "93340");
+INSERT INTO `address` VALUES (6,"16 rue conrad adenauer","rosny-sous-bois","93110"),
+(5,"2 rue corentin cariou","paris","75014"),
+(2,"2 rue du dépôt","compiègne","60200"),
+(3,"3 avenue de la résistance","le raincy","93340"),
+(1,"30 rue de clamart","compiègne","60200"),
+(4,"avenue du général de gaulle","rosny-sous-bois","93110"),
+(7,"rue du dr schweitzer","compiègne","60200"),
+(8,"rue roger couttolenc","compiègne","60200");
 
-INSERT INTO Ad(yearBook, name, phone, category, address)
-VALUES
-(1, "club de football", "0150876022", 1, 1),
-(1, "fans de foot", "0145823600", 1, 2),
-(1, "révisions bac", "0682453399", 2, 3);
-
-CALL addAd( 1, 'Yo bébé', '0303030303', 'c', 'd', 'e', 'Hello' );
+INSERT INTO `ad` VALUES (1,"club de football","0150876022",1,1),
+(1,"fans de foot","0145823600",1,2),
+(1,"le majestic","0150687750",4,5),(1,"pic\'asso","0155789033",6,8),
+(1,"révisions bac","0682453399",2,3),(1,"rosny 2‎","0148546319",3,4),
+(1,"ugc rosny 2","0176647997",4,6),
+(1,"université de technologie de compiègne","0344234423",5,7),
+(1,"usec","0344234362",6,8);
